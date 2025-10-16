5f5a4318633bf26cbbb6edbb635ff49ee763a862
IsmailMustopha <97275076+IsmailMustopha@users.noreply.github.com>
2025-10-16 04:18:10 -0700

    update

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -47,7 +47,7 @@ const clerkWebhooks = async (req, res) => {
     res.json({ success: true, message: "Webhook Received" });
   } catch (error) {
     console.log(error.message);
-    res.json({ success: true, message: error.message });
+    res.json({ success: false, message: error.message });
   }
 };
 
